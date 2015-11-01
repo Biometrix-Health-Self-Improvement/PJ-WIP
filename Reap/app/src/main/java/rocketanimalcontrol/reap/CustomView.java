1ec3653d3a966569a5e8e741faa7c8967237cfc3
JayGarxP <jpfallon4@gmail.com>
2015-10-31 22:39:03 -0700

    Got sound working! BUT major setback...
    XML Sprout is fast, easy, NON-REUSABLE and UGLY.
    Custom View class Sprout could potentially be PERFECT, but really HARD
    to implement. (Have to make own event handlers and buttons etc.)

--- Reap/app/src/main/java/rocketanimalcontrol/reap/CustomView.java
+++ Reap/app/src/main/java/rocketanimalcontrol/reap/CustomView.java
@@ -4,22 +4,25 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 
 public class CustomView extends View{
+
+    boolean mDownTouch = false;
 
     private SpriteAnimation animation;
 
     public CustomView(Context context) {
         super(context);
 
-
     }
+
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.BLACK);
+        canvas.drawColor(Color.MAGENTA);
         canvas.drawBitmap(animation.getCurrentFrame(), 300, 200, new Paint());
         animation.update();
         //NOT thread safe
@@ -32,6 +35,12 @@ public class CustomView extends View{
         this.postInvalidate();
     }
 
+  //Making buttons work.
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+      return super.onTouchEvent(event);
+  }
+    //http://developer.android.com/reference/android/view/GestureDetector.SimpleOnGestureListener.html
 
     public void setAnimation(SpriteAnimation animation)
     {
--- Reap/app/src/main/java/rocketanimalcontrol/reap/MainActivity.java
+++ Reap/app/src/main/java/rocketanimalcontrol/reap/MainActivity.java
@@ -1,35 +1,33 @@
 package rocketanimalcontrol.reap;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity; //? This needed with AppCompatActivity ?
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Rect;
+import android.widget.Button;
+//import android.graphics.Rect;
 import android.view.View;
-import android.view.Menu;
+//import android.view.Menu;
 //import android.view.animation.Animation;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity// implements View.OnClickListener
+{
+    //Animation, bitmaps, and canvas for Sprout
     private ArrayList<Bitmap> bitmaps;//new here or in OnCreate?
     private SpriteAnimation animation;
     private CustomView view;
-
-    //Auto-generated code
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
+    //Sound data for Sprout
+    public SoundPool mySound;
+    int cutebroopId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = new CustomView(this);
-
 
         bitmaps = new ArrayList<Bitmap>();
 
@@ -43,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
         animation = new SpriteAnimation(bitmaps, 60); //animation needs full bitmaps (fixed error from yesterday)
         view.setAnimation(animation);
         setContentView(view);
+
+
+        //Sound button code (deprecated)
+        mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        cutebroopId = mySound.load(this, R.raw.cutebroop, 1);
+
+        ////Sound button (NEED TO IMPLEMENT CUSTOM VIEW ONCLICK HANDLING FIRST)(Or fragment views...) //http://developer.android.com/training/custom-views/making-interactive.html
+       //setContentView(R.layout.activity_main); //uncomment this to see sound working w/o CustomView
+//       final Button playmySound = (Button) findViewById(R.id.button); //can't find button null
+//        playmySound.setOnClickListener (new View.OnClickListener() {
+//
+//            @Override //after CustomView done you'll be able to do http://www.mysamplecode.com/2011/10/android-programmatically-generate.html
+//            public void onClick(View v) {
+//                mySound.play(cutebroopId,1,1,1,0,1f);
+//            }
+//        });
+
+
+
+
+
+
+
 
     }
 //ERROR: cannot resolve menu symbol
